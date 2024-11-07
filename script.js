68f5c93074a6adef06c92627364cb655eb40e31a
cleiven <91392513+cleiven@users.noreply.github.com>
2024-11-06 20:30:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    console.log("testing for the cube");
+    if (document.cookie != "") {
+        console.log("cookie");
+        console.log(document.cookie);
+        //window.location.href="cube.html";
+    }
+}
